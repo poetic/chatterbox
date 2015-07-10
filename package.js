5baedd8eb391f12d32175ebda51f8d704ec1ebdc
Leonidez Acosta <leonidez@poeticsystems.com>
2015-07-10 00:26:09 -0500

    initial commit

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'poetic:chatterbox',
+  version: '0.0.1',
+  summary: 'chat bot for meteor apps',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+
+  api.use('matb33:collection-hooks@0.7.13');
+  api.use('grigio:babel@0.1.4');
+
+  api.imply('matb33:collection-hooks@0.7.13');
+  api.addFiles('chatterbox.es6.js', ['server']);
+  api.export('Chatterbox');
+});
