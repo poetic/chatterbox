dbca91e4e06064c69d30dac1905390e72f2fe827
Leonidez Acosta <leonidez@poeticsystems.com>
2015-09-20 18:11:26 -0500

    update babel dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:chatterbox',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'chat bot for meteor apps',
   git: 'https://github.com/poetic/chatterbox',
   documentation: 'README.md'
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2')
 
   api.use('matb33:collection-hooks@0.7.13')
-  api.use('grigio:babel@0.1.4')
+  api.use('grigio:babel@0.1.8')
 
   api.imply('matb33:collection-hooks@0.7.13')
   api.addFiles('chatterbox.es6.js', ['server'])
